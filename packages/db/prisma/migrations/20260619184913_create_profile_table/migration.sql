49860c6ead7a6d523715156cd514d4da892f7840
Noah Korner <noah.korner@rsmus.com>
2026-06-19 13:51:01 -0500

    fix: Baselines migrations

---
+++ packages/db/prisma/migrations/20260619184913_create_profile_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
